618a952df645a430267c546e25b9f24e5e7d7280
Edcarlo Lima <edcarlo.lima@institutoprosaber.com.br>
2020-03-11 17:52:48 -0300

    Criando model e tabela artigo

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -13,19 +13,39 @@
             ></tabela-lista>
         </painel>
     </pagina>
-    <modal nome="adicionar">
-        <painel titulo="Adicionar">
-            <formulario css="" action="#" method="put" enctype="" token="">
-                <div class="form-group">
-                    <label for="titulo">Título</label>
-                    <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título">
-                </div>
-                <div class="form-group">
-                    <label for="descricao">Descrição</label>
-                    <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">
-                </div>
-                <button class="btn btn-info">Adicionar</button>
-            </formulario>
-        </painel>
+    <modal nome="adicionar" titulo="Adicionar">
+        <formulario id="formAdicionar" css="" action="#" method="put" enctype="" token="">
+            <div class="form-group">
+                <label for="titulo">Título</label>
+                <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título">
+            </div>
+            <div class="form-group">
+                <label for="descricao">Descrição</label>
+                <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">
+            </div>
+        </formulario>
+        <span slot="botoes">
+            <button form="formAdicionar" class="btn btn-info">Adicionar</button>
+        </span>
+    </modal>
+
+    <modal nome="editar" titulo="Editar">
+        <formulario id="formEditar" css="" action="#" method="put" enctype="multipart/form-data" token="123456">
+            <div class="form-group">
+                <label for="ed-titulo">Título</label>
+                <input type="text" class="form-control" id="ed-titulo" name="titulo" v-model="$store.state.item.titulo" placeholder="Título">
+            </div>
+            <div class="form-group">
+                <label for="ed-descricao">Descrição</label>
+                <input type="text" class="form-control" id="ed-descricao" name="descricao" v-model="$store.state.item.descricao" placeholder="Descrição">
+            </div>
+        </formulario>
+        <span slot="botoes">
+            <button form="formEditar" class="btn btn-info">Editar</button>
+        </span>
+    </modal>
+
+    <modal nome="detalhe" v-bind:titulo="$store.state.item.titulo">
+        <p>@{{$store.state.item.descricao}}</p>
     </modal>
 @endsection
