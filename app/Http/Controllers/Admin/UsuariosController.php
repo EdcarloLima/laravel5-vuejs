3b7de88b90bf3cb881d7e934dde59498f657d4fc
Edcarlo Lima <edcarlo.lima@institutoprosaber.com.br>
2020-03-13 14:49:34 -0300

    crud de usuario

---
+++ app/Http/Controllers/Admin/UsuariosController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UsuariosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $listaMigalhas = json_encode([
+            ["titulo" => "Home", "url" => route('home')],
+            ["titulo" => "Lista de Usuários", "url" => ""]
+        ]);
+        $usuario = new User();
+        $listaUsuarios = $usuario->select('id','name','email')->paginate(2);
+        return response()->view('admin.usuarios.index', compact('listaMigalhas','listaUsuarios'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $usuario = new User();
+        $dados = $request->all();
+        $validacao = \Validator::make($dados,[
+            "name" => "required|string|max:255",
+            "email" => "required|string|email|max:255|unique:users",
+            "password" => "required|string|min:6",
+        ]);
+        if ($validacao->fails()) {
+            return redirect()->back()->withErrors($validacao)->withInput();
+        }
+        $dados['password'] = bcrypt($dados['password']);
+        $usuario->create($dados);
+        return response()->redirectToRoute('usuarios.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $usuario = new User();
+        return $usuario->find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $usuario = new User();
+        $dados = $request->all();
+        $validacao = \Validator::make($dados,[
+            "name" => "required|string|max:255",
+            "email" => "required|string|email|max:255|unique:users",
+            "password" => "required|string|min:6",
+        ]);
+        if ($validacao->fails()) {
+            return redirect()->back()->withErrors($validacao)->withInput();
+        }
+        $usuario->find($id)->update($dados);
+        return response()->redirectToRoute('usuarios.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $usuario = new User();
+        $usuario->find($id)->delete();
+        return response()->redirectToRoute('usuarios.index');
+    }
+}
